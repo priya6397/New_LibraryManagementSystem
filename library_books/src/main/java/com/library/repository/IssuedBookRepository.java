ba086a7623ee83d95bf9aeff99e4dcf1115289b8
Priya Mittal <priyamittal770@gmail.com>
2024-07-11 00:16:26 +0530

    Backend Updated

---
+++ library_books/src/main/java/com/library/repository/IssuedBookRepository.java
@@ -0,0 +1,15 @@
+package com.library.repository;
+
+import com.library.model.IssuedBook;
+import com.library.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IssuedBookRepository extends JpaRepository<IssuedBook,Long> {
+    List<IssuedBook> findByUserId(Long userId);
+
+    List<IssuedBook> findByUserAndIsReturnedFalse(User user);
+    void deleteAllByUser(User user);
+    List<IssuedBook> findByUserAndIsReturnedTrue(User user);
+}
